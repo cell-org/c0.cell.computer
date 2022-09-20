1890a9257a26c4b53f87be27975a929ac44edefe
skøgard <skogard@protonmail.com>
2022-09-20 15:11:45 -0400

    goerli

--- v0/config.js
+++ v0/config.js
@@ -2,6 +2,7 @@ const Config = {
   factory: "0x99Ba4B0aA5dD3fa399Ce04aCa79459AcE1c0460c",
   explorers: {
     "1": "https://etherscan.io/address/{address}",
-    "4": "https://rinkeby.etherscan.io/address/{address}"
+    "4": "https://rinkeby.etherscan.io/address/{address}",
+    "5": "https://goerli.etherscan.io/address/{address}"
   }
 }
